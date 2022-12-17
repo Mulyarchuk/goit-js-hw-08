c2f2fdc8bf9bbd1df730ec807c5e673d4f8ea704
Mulyarchuk Elena <ml.mulyarchuk@gmail.com>
2022-12-17 15:31:15 +0300

    ggg

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+const gallery = document.querySelector(`.gallery`);
+
+const markup = galleryItems.reduce((acc, {preview,original,description}) => 
+acc + `<div class="gallery__item">
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`, ``);
+
+gallery.insertAdjacentHTML(`beforeend`,markup);
+
+var lightbox = new SimpleLightbox('.gallery a', {captionsData: `alt`, captionDelay: `250ms` });
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(`.feedback-form`);
+
+const FEEDBACK_KEY = `feedback-form-state`;
+const formData = {};
+
+form.addEventListener(`submit`, onFormSubmit);
+form.addEventListener(`input`, throttle(onInput, 500));
+
+
+populateInput();
+
+function onFormSubmit (evt){
+ evt.preventDefault();
+ evt.currentTarget.reset();
+ localStorage.removeItem(FEEDBACK_KEY);
+ console.log(formData)
+};
+
+function onInput (evt){
+
+localStorage.setItem (FEEDBACK_KEY, JSON.stringify(formData))
+
+formData [evt.target.name] = evt.target.value;
+console.log (formData)
+
+};
+function getLocalStorageData(){
+   return JSON.parse(localStorage.getItem(FEEDBACK_KEY))
+}
+
+function populateInput(){
+    const saveMessage = getLocalStorageData();
+    if (saveMessage){
+        form.value = saveMessage;
+    }
+    console.log (saveMessage)
+    
+}
