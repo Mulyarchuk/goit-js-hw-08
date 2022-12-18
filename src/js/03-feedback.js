c1134cd04c5a9f526df597c8e04af8ebdf5912ed
Mulyarchuk Elena <ml.mulyarchuk@gmail.com>
2022-12-18 14:39:54 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,32 +6,51 @@ const input = document.querySelector(`.feedback-form input`);
 const textarea = document.querySelector(`.feedback-form textarea`);
 
 const FEEDBACK_KEY = `feedback-form-state`;
-const formData = {};
+// const formData = {};
 
 form.addEventListener(`submit`, onFormSubmit);
 input.addEventListener(`input`, throttle(onInput, 500));
 textarea.addEventListener(`input`, throttle(onInput, 500));
 
+const formData = {};
+// function onFormSubmit (evt){
+//  evt.preventDefault();
+//  form.reset();
+//  localStorage.removeItem(FEEDBACK_KEY);
+//  console.log(formData);
 
+// };
+
+// function onInput (evt){
+
+// localStorage.setItem (FEEDBACK_KEY, JSON.stringify(formData));
+
+// formData [evt.target.name] = evt.target.value;
+// console.log (formData)
+
+// };
+
+//  saveMessage= JSON.parse(localStorage.getItem(FEEDBACK_KEY)) || {};
+//     if(formData){
+//         input.value = saveMessage.email|| ``;
+//         textarea.value = saveMessage.message|| ``;
+//     };
+  
+
+function onInput (evt) {
+    
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
+    formData [evt.target.name] = evt.target.value;
+}
 
 function onFormSubmit (evt){
- evt.preventDefault();
- form.reset();
- localStorage.removeItem(FEEDBACK_KEY);
- console.log(formData)
-};
-
-function onInput (evt){
-
-localStorage.setItem (FEEDBACK_KEY, JSON.stringify(formData))
-
-formData [evt.target.name] = evt.target.value;
-console.log (formData)
-
-};
- saveMessage= JSON.parse(localStorage.getItem(FEEDBACK_KEY)) || {};
-    if(formData){
-        input.value = saveMessage.email|| ``;
-        textarea.value = saveMessage.message||``;
-    }
-    
+    evt.preventDefault();
+    console.log(formData);
+    form.reset();
+    localStorage.removeItem(FEEDBACK_KEY);
+}
+saveMessage= JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+    if(saveMessage){
+        input.value = saveMessage.email;
+        textarea.value = saveMessage.message;
+    };
